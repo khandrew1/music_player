2494705c43856300147f065b66254916c21f9d11
Andrew K <andrewkh15@gmail.com>
2022-07-13 00:22:31 -0700

    updated music_player.py to use grid layout

--- code/music_player.py
+++ code/music_player.py
@@ -27,6 +27,15 @@ class DirectorySelector(Widget):
     def on_leave(self) -> None:
         self.mouse_over = False
 
+# TODO: Refactor NowPlaying into its own file as well
+class NowPlaying(Widget):
+
+    def render(self) -> Panel:
+        return Panel(Align.center("Nothing Playing", vertical="middle"))
+
+    def update(self, mf) -> None:
+        return Panel(Align.center(mf, vertical="middle"))
+
 class MusicPlayer(App):
 
     global player
@@ -42,24 +51,47 @@ class MusicPlayer(App):
         player.pause()
 
     async def on_mount(self) -> None:
+        # Creating Grid
+
+        grid = await self.view.dock_grid(edge="left", name="left")
+
+        grid.add_column(size=48, name="left", min_size=20)
+        grid.add_column(size=30, name="center") 
+        grid.add_column(fraction=1, name="right")
+
+        grid.add_row(size=3, name="top", min_size=2)
+        grid.add_row(fraction=1, name="middle")
+        grid.add_row(size=10, name="button")
+        grid.add_row(size=1, name="bottom") 
+
+
+        grid.add_areas(
+                area1="left-start|right-end,top",
+                area2="left-start|right-end,bottom",
+                area3="left,middle",
+                area4="left,button",
+                area5="left-end|right-end,middle",
+                area6="left-end|right-end,button",
+        )
+
+
+
         # Creating Widgets
-        self.body = ScrollView(Align.center("Nothing Playing", vertical="middle"))
+
+        # TODO: Change body from ScrollView() to custom panel widget
+        self.body = ScrollView(Align.center("Nothing Playing", vertical="middle")) 
 
         # TODO: UN-hard code directory
         self.directory = DirectoryTree("/home/andrxw/Downloads", "Music") # shows the directory
         
-        await self.view.dock(Header(), edge="top") # Docks the header
-        self.title = "Music Player" # Sets title on top of header
-
-        await self.view.dock(Footer(), edge="bottom") # Docks Footer
-
-        # await self.view.dock(Placeholder(), edge="bottom", size=10)
-
-        await self.view.dock(DirectorySelector(), edge="bottom", size=10)
-
-        await self.view.dock(ScrollView(self.directory), edge="left", size=48, name="Sidebar") # docks the directory and allows it to scroll
- 
-        await self.view.dock(self.body, edge="top") # docks body which will display the currently playing music
+        grid.place(
+                area1=Header(),
+                area2=Footer(),
+                area3=ScrollView(self.directory),
+                area4=DirectorySelector(), 
+                area5=self.body,
+                area6=Placeholder(),
+        )
 
     async def handle_file_click(self, message: FileClick) -> None:
         # Message sent by directory tree when clicked
